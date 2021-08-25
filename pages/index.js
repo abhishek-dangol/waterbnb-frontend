a036bc2729367388d6143f7ea62f68a2473ccf7f
abhishek-dangol <abhi.dangol@gmail.com>
2021-08-24 23:44:35 -0500

    created slug page; received props from backend

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,31 @@
+import { sanityClient } from "../sanity"
 
-export default function Home() {
+
+export default function Home({ properties }) {
+  console.log(properties);
   return (
     <>
       
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "property"]'
+  const properties = await sanityClient.fetch(query);
+
+  if (!properties.length) {
+    return {
+      props: {
+        properties: [],
+      }
+    }
+  } else {
+    return {
+      props: {
+        properties
+      }
+    }
+  }
+
 }
